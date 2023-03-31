f6e5b882302d92f8a24d5dc7afce2cf5752e58f9
Abdullah Öztürk <abdullahozturk99@gmail.com>
2023-03-31 11:46:56 +0300

    feat: init repository interfaces

---
+++ Core/Application/Clicco.Application/Interfaces/Repositories/IGenericRepository.cs
@@ -0,0 +1,17 @@
+﻿using Clicco.Domain.Core;
+using System.Linq.Expressions;
+
+namespace Clicco.Application.Interfaces.Repositories
+{
+    public interface IGenericRepository<T> where T : BaseEntity
+    {
+        Task<List<T>> GetAll();
+        Task<List<T>> Get(Expression<Func<T, bool>> filter = null, params Expression<Func<T, object>>[] includes);
+        Task<T> GetById(int id);
+        Task<T> GetByIdAsync(int id, params Expression<Func<T, object>>[] includes);
+        Task<T> GetSingleAsync(Expression<Func<T, bool>> expression, params Expression<Func<T, object>>[] includes);
+        Task<T> AddAsync(T entity);
+        T Update(T entity);
+        Task<bool> DeleteAsync(int id);
+    }
+}
---
+++ Core/Application/Clicco.Application/Interfaces/Repositories/ITransactionRepository.cs
@@ -0,0 +1,9 @@
+﻿using Clicco.Domain.Model;
+
+namespace Clicco.Application.Interfaces.Repositories
+{
+    public interface ITransactionRepository : IGenericRepository<Transaction>
+    {
+
+    }
+}
