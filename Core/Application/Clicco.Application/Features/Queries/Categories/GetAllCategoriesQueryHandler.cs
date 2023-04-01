eac6043747cd4fcdd6d054d22194bca0817942fa
Abdullah Öztürk <abdullahozturk99@gmail.com>
2023-04-01 19:27:31 +0300

    feat: get all categories feature has been created

--- Core/Application/Clicco.Application/Features/Queries/Categories/GetAllCategoriesQueryHandler.cs
+++ Core/Application/Clicco.Application/Features/Queries/Categories/GetAllCategoriesQueryHandler.cs
@@ -1,6 +1,24 @@
-﻿namespace Clicco.Application.Features.Queries
+﻿using Clicco.Application.Interfaces.Repositories;
+using Clicco.Domain.Model;
+using MediatR;
+
+namespace Clicco.Application.Features.Queries
 {
-    public class GetAllCategoriesQueryHandler
+    public class GetAllCategoriesQuery : IRequest<List<Category>>
     {
+
+    }
+
+    public class GetAllCategoriesQueryHandler : IRequestHandler<GetAllCategoriesQuery, List<Category>>
+    {
+        private readonly ICategoryRepository categoryRepository;
+        public GetAllCategoriesQueryHandler(ICategoryRepository categoryRepository)
+        {
+            this.categoryRepository = categoryRepository;
+        }
+        public async Task<List<Category>> Handle(GetAllCategoriesQuery request, CancellationToken cancellationToken)
+        {
+            return await categoryRepository.GetAll();
+        }
     }
 }
