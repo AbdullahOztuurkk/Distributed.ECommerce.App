b1e611f42ae55743fec153dd03f5b176c1d547b5
Abdullah Öztürk <abdullahozturk99@gmail.com>
2023-04-02 18:59:34 +0300

    fix: accessibility modifiers of  base response model

--- Core/Domain/Clicco.Domain.Core/ResponseModel/BaseResponse.cs
+++ Core/Domain/Clicco.Domain.Core/ResponseModel/BaseResponse.cs
@@ -4,19 +4,19 @@
     {
         public bool IsSuccess { get; private set; }
         public string ErrorMessage { get; private set; }
-        private BaseResponse(bool IsSuccess, string message)
+        protected BaseResponse(bool IsSuccess, string message)
         {
             this.IsSuccess = IsSuccess;
             ErrorMessage = message;
         }
-        public class ErrorResponse : BaseResponse
-        {
-            public ErrorResponse(string Message) : base(false, Message) { }
-        }
+    }
+    public class ErrorResponse : BaseResponse
+    {
+        public ErrorResponse(string Message) : base(false, Message) { }
+    }
 
-        public class SuccessResponse : BaseResponse
-        {
-            public SuccessResponse(string Message) : base(true, Message) { }
-        }
+    public class SuccessResponse : BaseResponse
+    {
+        public SuccessResponse(string Message) : base(true, Message) { }
     }
 }
