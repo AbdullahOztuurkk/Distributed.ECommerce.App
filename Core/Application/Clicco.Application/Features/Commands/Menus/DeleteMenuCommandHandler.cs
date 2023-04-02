c79448420d6a7193876c2d172a94d0cb1c333ac5
Abdullah Öztürk <abdullahozturk99@gmail.com>
2023-04-02 16:01:50 +0300

    feat: delete menu command has been created

---
+++ Core/Application/Clicco.Application/Features/Commands/Menus/DeleteMenuCommandHandler.cs
@@ -0,0 +1,35 @@
+﻿using Clicco.Application.Features.Queries;
+using Clicco.Application.Interfaces.Repositories;
+using Clicco.Domain.Core.ResponseModel;
+using MediatR;
+using static Clicco.Domain.Core.ResponseModel.BaseResponse;
+
+namespace Clicco.Application.Features.Commands
+{
+    internal class DeleteMenuCommand : IRequest<BaseResponse>
+    {
+        public int Id { get; set; }
+    }
+
+    internal class DeleteMenuCommandHandler : IRequestHandler<DeleteMenuCommand, BaseResponse>
+    {
+        private readonly IMenuRepository menuRepository;
+        private readonly IMediator mediator;
+        public DeleteMenuCommandHandler(IMenuRepository menuRepository, IMediator mediator)
+        {
+            this.menuRepository = menuRepository;
+            this.mediator = mediator;
+        }
+        public async Task<BaseResponse> Handle(DeleteMenuCommand request, CancellationToken cancellationToken)
+        {
+            var menu = await mediator.Send(new GetMenuByIdQuery { Id = request.Id }, cancellationToken);
+            if (menu != null)
+            {
+                await menuRepository.DeleteAsync(menu);
+                await menuRepository.SaveChangesAsync();
+                return new SuccessResponse("Menu has been deleted!");
+            }
+            return new ErrorResponse("Menu not found!");
+        }
+    }
+}
