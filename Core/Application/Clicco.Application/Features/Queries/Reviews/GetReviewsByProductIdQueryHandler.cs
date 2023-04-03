d81a57c7c4965041f978b49fc9ee0c92a3c19580
Abdullah Öztürk <abdullahozturk99@gmail.com>
2023-04-03 10:24:16 +0300

    feat: get review by product feature has been added

---
+++ Core/Application/Clicco.Application/Features/Queries/Reviews/GetReviewsByProductIdQueryHandler.cs
@@ -0,0 +1,31 @@
+﻿using Clicco.Application.Interfaces.Repositories;
+using Clicco.Domain.Model;
+using MediatR;
+
+namespace Clicco.Application.Features.Queries
+{
+    public class GetReviewsByProductIdQuery : IRequest<List<Review>>
+    {
+        public int ProductId { get; set; }
+    }
+
+    public class GetReviewsByProductIdQueryHandler : IRequestHandler<GetReviewsByProductIdQuery, List<Review>>
+    {
+        private readonly IReviewRepository reviewRepository;
+        private IMediator mediator;
+        public GetReviewsByProductIdQueryHandler(IReviewRepository reviewRepository, IMediator mediator)
+        {
+            this.reviewRepository = reviewRepository;
+            this.mediator = mediator;
+        }
+        public async Task<List<Review>> Handle(GetReviewsByProductIdQuery request, CancellationToken cancellationToken)
+        {
+            var product = await mediator.Send(new GetProductByIdQuery { Id = request.ProductId },cancellationToken);
+            if(product == null)
+            {
+                throw new Exception("Product not Found!");
+            }
+            return await reviewRepository.Get(x => x.ProductId == request.ProductId, x => x.User);
+        }
+    }
+}
