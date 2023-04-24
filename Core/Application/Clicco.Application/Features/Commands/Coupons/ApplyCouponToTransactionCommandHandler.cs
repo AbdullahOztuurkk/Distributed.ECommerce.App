b3d85ab0b74b355d898f7d51ccd72ad9b9bf7113
Abdullah Öztürk <abdullahozturk99@gmail.com>
2023-04-24 20:19:48 +0300

    feat: cache mechanism  has been added after applying coupon operation

--- Core/Application/Clicco.Application/Features/Commands/Coupons/ApplyCouponToTransactionCommandHandler.cs
+++ Core/Application/Clicco.Application/Features/Commands/Coupons/ApplyCouponToTransactionCommandHandler.cs
@@ -1,5 +1,7 @@
-﻿using Clicco.Application.Interfaces.Repositories;
+﻿using Clicco.Application.Interfaces.CacheManager;
+using Clicco.Application.Interfaces.Repositories;
 using Clicco.Application.Interfaces.Services;
+using Clicco.Domain.Core;
 using Clicco.Domain.Core.ResponseModel;
 using MediatR;
 
@@ -14,11 +16,16 @@ namespace Clicco.Application.Features.Commands
     public class ApplyCouponToTransactionCommandHandler : IRequestHandler<ApplyCouponToTransactionCommand, BaseResponse>
     {
         private readonly ICouponRepository couponRepository;
+        private readonly ICacheManager cacheManager;
         private readonly ICouponService couponService;
-        public ApplyCouponToTransactionCommandHandler(ICouponRepository couponRepository, ICouponService couponService)
+        public ApplyCouponToTransactionCommandHandler(
+            ICouponRepository couponRepository,
+            ICouponService couponService,
+            ICacheManager cacheManager)
         {
             this.couponRepository = couponRepository;
             this.couponService = couponService;
+            this.cacheManager = cacheManager;
         }
         public async Task<BaseResponse> Handle(ApplyCouponToTransactionCommand request, CancellationToken cancellationToken)
         {
@@ -30,6 +37,13 @@ namespace Clicco.Application.Features.Commands
             await couponService.IsAvailable(request.TransactionId, coupon);
 
             await couponService.Apply(request.TransactionId, coupon);
+
+            var activeCoupons = await cacheManager.GetListAsync(CacheKeys.ActiveCoupons);
+
+            if(!activeCoupons.Any(x => x == coupon.Id.ToString()))
+            {
+                await cacheManager.AddToListAsync(CacheKeys.ActiveCoupons, coupon.Id.ToString());
+            }
 
             return new SuccessResponse("Coupon has been used successfully!");
 
