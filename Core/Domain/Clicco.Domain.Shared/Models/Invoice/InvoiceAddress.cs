ce5c6fb198d48783f78da585e46e2473e3edbb39
Abdullah Öztürk <abdullahozturk99@gmail.com>
2023-05-04 01:05:13 +0300

    feat: invoice address display attributes has been added

--- Core/Domain/Clicco.Domain.Shared/Models/Invoice/InvoiceAddress.cs
+++ Core/Domain/Clicco.Domain.Shared/Models/Invoice/InvoiceAddress.cs
@@ -1,11 +1,22 @@
-﻿namespace Clicco.Domain.Shared.Models.Invoice
+﻿using static Clicco.Domain.Shared.Global;
+
+namespace Clicco.Domain.Shared.Models.Invoice
 {
     public class InvoiceAddress
     {
+        [DisplayElement("#ADDRESS_CITY#")]
         public string City { get; set; }
+
+        [DisplayElement("#ADDRESS_STREET#")]
         public string Street { get; set; }
+
+        [DisplayElement("#ADDRESS_STATE#")]
         public string State { get; set; }
+
+        [DisplayElement("#ADDRESS_COUNTRY#")]
         public string Country { get; set; }
+
+        [DisplayElement("#ADDRESS_ZIPCODE#")]
         public string ZipCode { get; set; }
     }
 }
